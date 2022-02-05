b3c3080da12e2c4e70ceeaa694abc7bcbc09c724
krishnajhapate <ki54.jhapate@gmail.com>
2022-02-05 19:13:29 +0530

    linear search in array

--- src/com/krishna/LinearSearch.java
+++ src/com/krishna/LinearSearch.java
@@ -6,10 +6,13 @@ public class LinearSearch {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 24234, 3, 5, 5, 3, 23, 42, 23, 432, 52, -24, 23 };
         int target = 2;
-        int res = linearSearch(nums, target);
+        // int res = linearSearch(nums, target);
+        // int res = linearSearch2(nums, target);
+        boolean res = linearSearch3(nums, target);
         System.out.println(res);
     }
 
+    // linear search for array
     static int linearSearch(int[] arr, int ser) {
         if (arr.length == 0) {
             return -1;
@@ -20,5 +23,31 @@ public class LinearSearch {
             }
         }
         return -1;
+    }
+
+    // search the target and return the element
+    static int linearSearch2(int[] arr, int ser) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ser) {
+                return ser;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    // search the element and return false or true
+    static boolean linearSearch3(int[] arr, int ser) {
+        if (arr.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ser) {
+                return true;
+            }
+        }
+        return false;
     }
 }
